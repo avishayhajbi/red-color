a8538f998aee0e34a967e1cbd205821d2ea9994c
Vidran Abdovich <vandervidi@gmail.com>
2014-07-21 19:01:10 +0300

    testing map zomming

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function initMapCss(){
 	$("#map").css('width',$(window).width()+"px");
 	$("#map").css('height',$(window).height()+"px");
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function setAlarm(lon, lat, time) {
@@ -30,28 +31,32 @@ function setAlarm(lon, lat, time) {
 	};
 	// Add the circle for this city to the map.
 	var cityCircle = new google.maps.Circle(alarmPosition);
-	//trying to remove the circle after some time
+	//remove the circle after some time
 	setTimeout(function() {
 		cityCircle.setMap(null);
 	}, time * 1000);
 }
 
+
 var refreshData = function() {
+	//Retrieveing the Json data output from Pikud Ha Oref
 	$.ajax({
 		url : "http://www.shenkar.info/vidran/index.php",
 		type : 'GET',
-		//async: false,
-		//cache: false,
-		//dataType: "json",
 		success: function(res) {
 			// get regions array from Pikud Ha Oref
 			res = res.split(",");
-			/*if(res[0]==""){
-				res="";
-				setTimeout(function(){map.setZoom(10);
-				map.setCenter(new google.maps.LatLng(centerLan, centerLon));},25000);
-			}*/
+			
+			//If the Json data output string from Pikud Ha Oref returns an empty arrey then set zoom to the original value ,10.
+			if(res[0]==""){
+							res="";
+							setTimeout(function(){map.setZoom(10);
+							//map.setCenter(new google.maps.LatLng(centerLan, centerLon));
+							},5000);
+						}
+			//if Json data string output string from Pikud Ha Oref is not empty
 			if (res.length)
+			console.log(res.length);
 				$.each(res, function(i, region) {
 					$.ajax({
 						url : "regions.json",
@@ -62,18 +67,26 @@ var refreshData = function() {
 							$.each(res, function(i, item) {
 								if (item.region == region) {
 									$.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address= ' + item.city + '&language=he&key=AIzaSyBeOwVCCs-P50VpNqTL1Dd_5iRivtJ4I0A', function(res) {
+										console.log(res.length);
+										if(res.length>2){
+											//Setting a new center according to the latest alarm
+											map.setZoom(11);
+										}
+										else
+										{//Setting a new center according to the latest alarm
+											map.setZoom(10);
+										}
+										
 										// for each city get the coordinates from google
 										$.each(res.results, function(i, f) {
 											var tmp = "" + item.city + ", ישראל";
 											if (tmp == f.formatted_address) {
-												//console.log(tmp + " --- " + f.formatted_address);
-												//console.log(item.time);
+												//Creating new alarm
 												setAlarm(f.geometry.location.lat, f.geometry.location.lng, item.time);
-												//Setting a new center according to the latest alarm
-												map.setZoom(12);
-												// This will trigger a zoom_changed on the map
+												// This will set the map center to the center of the alarm location
 												map.setCenter(new google.maps.LatLng(f.geometry.location.lat, f.geometry.location.lng));
-												//map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+												//Return the zoom attribute to its original value after the alarm.
+												//setTimeout(function(){map.setCenter(new google.maps.LatLng(centerLan, centerLon));map.setZoom(10);},item.time*500);
 											}
 										});
 									});
